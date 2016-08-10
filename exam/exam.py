7f522751f7eb6d4dd140c8df910a080ab4f2f27b
tsuyoshi <tsuyoshi.nakasone@gmail.com>
2016-08-10 12:06:32 +0900

    問題追加 latest 1

--- exam/exam.py
+++ exam/exam.py
@@ -21,3 +21,53 @@ def number_sign(number):
 # number が素数なら'ok',そうでないなら'ng'と出力してください
 def prime_number(number):
     pass
+
+
+# 1からnumberまでの合計を出力してください
+def sum_from_1_to(number):
+    pass
+
+
+# numberの階乗(factorial)を出力してください
+def factorial(number):
+    pass
+
+
+# リストdataの各要素(整数)を3乗した結果をリスト型として返してください
+def cubic_list(data):
+    pass
+
+
+# 底辺x,高さyの直角三角形(right angled triangle)の残り1つの辺の長さを返してください
+def calc_hypotenuse(x, y):
+    pass
+
+
+# 底辺x,斜辺vの直角三角形の残り1つの辺の長さを返してください
+def calc_subtense(x, v):
+    pass
+
+
+# 三辺の長さがそれぞれx,y,zの三角形の面積を返してください
+def calc_area_triangle(x, y, z):
+    pass
+
+
+# 引数a,b,cを小数点以下2桁表示で空白切りで表示してください
+def point_two_digits(a, b, c):
+    pass
+
+
+# リストdataの内容を小さい順でソートした結果を返してください
+def list_sort(data):
+    pass
+
+
+# 文字列の並びを逆にしたものを返してください
+def reverse_string(sentence):
+    pass
+
+
+# dateから2016年4月1日までの日数を返してください
+def days_from_date(point):
+    pass
--- test_exam.py
+++ test_exam.py
@@ -1,6 +1,8 @@
 import unittest
 import sys
+import math
 from io import StringIO, SEEK_SET
+from datetime import date
 
 from exam import exam, test
 
@@ -68,7 +70,6 @@ class ExamTest(unittest.TestCase, StdIOHook):
         exam.slicing2to5('Python Tutorial')
         self.assertEqual(sys.stdout.getvalue(), 'tho\n')
 
-
     def test_number_sign(self):
         exam.number_sign(-3)
         self.assertEqual(sys.stdout.getvalue(), '-\n')
@@ -109,3 +110,81 @@ class ExamTest(unittest.TestCase, StdIOHook):
         self.reset_stdout()
         exam.prime_number(11)
         self.assertEqual(sys.stdout.getvalue(), 'ok\n')
+
+    def test_sum_from_1_to(self):
+        exam.sum_from_1_to(1)
+        self.assertEqual(sys.stdout.getvalue(), '1\n')
+        self.reset_stdout()
+        exam.sum_from_1_to(5)
+        self.assertEqual(sys.stdout.getvalue(), '15\n')
+        self.reset_stdout()
+        exam.sum_from_1_to(1000)
+        self.assertEqual(sys.stdout.getvalue(), '500500\n')
+        self.reset_stdout()
+        exam.sum_from_1_to(777)
+        self.assertEqual(sys.stdout.getvalue(), '302253\n')
+        self.reset_stdout()
+
+    def test_factorial(self):
+        exam.factorial(0)
+        self.assertEqual(sys.stdout.getvalue(), '1\n')
+        self.reset_stdout()
+        exam.factorial(4)
+        self.assertEqual(sys.stdout.getvalue(), '24\n')
+        self.reset_stdout()
+        exam.factorial(9)
+        self.assertEqual(sys.stdout.getvalue(), '362880\n')
+        self.reset_stdout()
+        exam.factorial(13)
+        self.assertEqual(sys.stdout.getvalue(), '6227020800\n')
+        self.reset_stdout()
+        exam.factorial(7)
+        self.assertEqual(sys.stdout.getvalue(), '5040\n')
+        self.reset_stdout()
+
+    def test_cubic_list(self):
+        self.assertEqual(
+            exam.cubic_list([1, 2, 3, 4, 5]), [1, 8, 27, 64, 125])
+        self.assertEqual(exam.cubic_list(
+            [-3, -2, -1, 0, 1, 2, 3]), [-27, -8, -1, 0, 1, 8, 27])
+
+    def test_calc_hypotenuse(self):
+        self.assertEqual(exam.calc_hypotenuse(1, 2), math.sqrt(5))
+        self.assertEqual(exam.calc_hypotenuse(3, 4), 5.0)
+        self.assertEqual(exam.calc_hypotenuse(9, 12), 15.0)
+
+    def test_calc_subtense(self):
+        self.assertEqual(exam.calc_subtense(15, 12), 9)
+        self.assertEqual(exam.calc_subtense(5, 3), 4)
+        self.assertEqual(exam.calc_subtense(1, 2), math.sqrt(3))
+
+    def test_calc_area_triangle(self):
+        self.assertEqual(exam.calc_area_triangle(3, 4, 5), 6)
+        self.assertEqual(exam.calc_area_triangle(9, 12, 15), 54)
+        self.assertEqual(exam.calc_area_triangle(10, 15, 20), 72.61843774138907)
+
+    def test_point_two_digits(self):
+        exam.point_two_digits(1.4142135623730951, 1.7320508075688772, 2.23606797749979)
+        self.assertEqual(sys.stdout.getvalue(), '1.41 1.73 2.24\n')
+        self.reset_stdout()
+        exam.point_two_digits(2.449489742783178, 2.6457513110645907, 2.8284271247461903)
+        self.assertEqual(sys.stdout.getvalue(), '2.45 2.65 2.83\n')
+        self.reset_stdout()
+        exam.point_two_digits(2, 3, 4)
+        self.assertEqual(sys.stdout.getvalue(), '2.00 3.00 4.00\n')
+        self.reset_stdout()
+
+    def test_list_sort(self):
+        self.assertEqual(exam.list_sort([1, 2, 3, 4]), [1, 2, 3, 4])
+        self.assertEqual(exam.list_sort([4, 3, 2, 1]), [1, 2, 3, 4])
+        self.assertEqual(exam.list_sort([3, 3, 2, 9]), [2, 3, 3, 9])
+        self.assertEqual(exam.list_sort([-1, -2, -3, -4]), [-4, -3, -2, -1])
+
+    def test_reverse_string(self):
+        self.assertEqual(exam.reverse_string('hello, python'), 'nohtyp ,olleh')
+        self.assertEqual(exam.reverse_string('dive into python 3'), '3 nohtyp otni evid')
+
+    def test_days_from_date(self):
+        self.assertEqual(exam.days_from_date(date(1969, 12, 25)), 16899)
+        self.assertEqual(exam.days_from_date(date(2001, 4, 1)), 5479)
+        self.assertEqual(exam.days_from_date(date(2015, 4, 1)), 366)
